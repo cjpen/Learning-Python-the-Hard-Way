324e9d3bc4dfe920ec1c4979269b3710a4227bd2
cjpen <64857004+cjpen@users.noreply.github.com>
2020-05-05 12:06:21 -0700

    update 2

---
+++ ex02/ex2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// A comment, this is so you can read your program later.
+	// Anything after the // is ignored by go.
+
+	fmt.Println("I could have code like this.") // and the comment after is ignored.)
+
+	// You can also use a comment to "disable" or comment out a piece of code:
+	// fmt.Println("This won't run.")
+
+	fmt.Println("This will run.")
+}
